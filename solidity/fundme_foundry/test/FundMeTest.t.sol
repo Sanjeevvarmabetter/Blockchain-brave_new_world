0107933f980248110d02a4a64ed76f4e35f48c4f
Sanjeevvarmabettter <sanjeevvarmacode@gmail.com>
2024-01-13 18:12:50 +0530

    add test

---
+++ solidity/fundme_foundry/test/FundMeTest.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+
+//i am inheriting the test contract
+contract FundMetest is Test {
+        function setUp() external {
+
+        
+        }
+
+        function testDemo() public {
+
+        
+        }
+
+
+}
