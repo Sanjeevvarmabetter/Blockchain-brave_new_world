d28fbc2c663e886a570ebd368a255091baea1d7e
Sanjeevvarmabettter <sanjeevvarmacode@gmail.com>
2024-01-17 00:28:52 +0530

    continue this

--- solidity/hardhat-ethers/ethers-simple-storage/SimpleStorage.sol
+++ solidity/hardhat-ethers/ethers-simple-storage/SimpleStorage.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.7;
+pragma solidity ^0.8.7;
 
 
 contract SimpleStorage {
-    uint256 favoriteNumber;
+    uint256 favoriteNumber; 
 
     struct People {
         uint256 favoriteNumber;
