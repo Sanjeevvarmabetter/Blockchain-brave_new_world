9ac6c0fb9b9c626720f2ea54d7e51128824c075a
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-04-20 00:03:42 +0530

    added the  def

---
+++ zero-knowledge-proof/main.js
@@ -0,0 +1,8 @@
+//  program that uses a zero-knowledge proof to verify the correctness of a computation.
+
+
+//EXPLAINED
+
+/* it is a method by which one party can prove to another party that something is true,without revealing 
+the info apart from the fact this statement is true */
+    
