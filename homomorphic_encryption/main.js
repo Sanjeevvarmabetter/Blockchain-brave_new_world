166876f81d6b234a0ec6b17c0c5d7bce282f512a
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-04-27 01:38:38 +0530

    complete this

--- homomorphic_encryption/main.js
+++ homomorphic_encryption/main.js
@@ -1,1 +1,9 @@
 // program that uses a homomorphic encryption scheme to perform secure computations on encrypted data.
+
+
+// homomorphic encryption: this is a type of encrytion algorithm that allows users to perform mathematical operations 
+//                         on encrypted data without decrypting it 
+
+
+
+
