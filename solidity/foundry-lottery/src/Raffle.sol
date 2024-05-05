66ea0a76781e1faa7096f41d76436efba06589d0
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-05-06 00:41:09 +0530

    added

--- solidity/foundry-lottery/src/Raffle.sol
+++ solidity/foundry-lottery/src/Raffle.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
 
-
+ // i am going to use lot of commentt because i am still learning
 pragma solidity ^0.8.18;
 
 
 contract Raffle {
 
+    error notenoughEth();
 
     uint256 private immutable enterFee;
+
+    //use a dynamic array for storing the users
+    address[] private players; 
+
 
     constructor(uint256 EnterFee){ 
     enterFee = EnterFee;
@@ -15,6 +20,25 @@ contract Raffle {
 
     function enterRaffle() public payable {
             //the people have to pay for the ticket to enter the raffle
+        //use the enterfee function to let people pay for this
+
+        //note : require is not a gas efficent  use if statements
+        // require(msg.value >= enterFee,"Not enough eth,so pls try again");
+    // error and if is more gas efficeient 
+
+
+        if(msg.value < enterFee) {
+        revert notenoughEth();
+    }
+
+        //push into the players array
+
+        players.push(payable(msg.sender)); //this will push
+
+
+
+
+
 
     }
 
