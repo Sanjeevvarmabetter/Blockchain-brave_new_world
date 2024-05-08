91313aaf68824a6c56bd75e3ac5ad772fdd5ff87
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-05-09 03:39:43 +0530

    fix the errors

--- solidity/foundry-lottery/src/Raffle.sol
+++ solidity/foundry-lottery/src/Raffle.sol
@@ -3,15 +3,31 @@
  // i am going to use lot of commentt because i am still learning
 pragma solidity ^0.8.18;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+//download cmd $ forge install smartcontractkit/chainlink-brownie-contracts@0.6.1 --no-commit
 
 contract Raffle {
 
     error notenoughEth();
 
+
+    // state variable
+
+    uint256 private constant request_conformations = 3;
+    uint256 private constant num_words = 1;
+
+
+
     uint256 private immutable enterFee;
     uint256 private immutable lottery_interval;
     uint256 private lasttimestamp;
+    uint256 private immutable vrf_coordinator;
+    uint256 private immutable my_gaslane;
+    uint256 private immutable my_subsciptionid;
+    uint32 private immutable my_callbackgaslimit;
+    
     //use a dynamic array for storing the users
+    
     address[] private players; 
 
 
@@ -19,8 +35,16 @@ contract Raffle {
 
     event enteredRaffle(address indexed player);
 
-    constructor(uint256 EnterFee){ 
+    constructor(uint256 EnterFee,uint256 interval,address vrfcoordinator,bytes32 gasLane,uint64 subid,uint32 callbackgaslimit
+    ){ 
     enterFee = EnterFee;
+    lottery_interval = interval;
+    vrf_coordinator =VRFCoordinatorV2Interface(vrfcoordinator); //typecasting the interface
+    lasttimestamp = block.timestamp;    
+    my_gaslane = gasLane;
+    my_subsciptionid = subid;
+    my_callbackgaslimit = callbackgaslimit;
+
     }
 
     function enterRaffle() public payable {
@@ -61,6 +85,15 @@ contract Raffle {
         if(block.timestamp - lasttimestamp < lottery_interval) {
             revert();
         }
+
+
+        uint256 requestId = vrf_coordinator.requestRandomWords( //
+        my_gaslane,
+        my_subsciptionid,
+        request_conformations,
+        my_callbackgaslimit,
+        num_words
+    );
     }
 
 
